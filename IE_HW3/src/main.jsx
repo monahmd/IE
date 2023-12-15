0e5e8511ccd3b288836f48c241b30a627cc7430f
mona hm <72091600+monahmd@users.noreply.github.com>
2023-12-15 23:33:06 +0330

    Add files via upload

---
+++ IE_HW3/src/main.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App.jsx'
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App/>,
+    },
+    {
+        path: "/:page",
+        element: <App/>,
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+      <RouterProvider router={router}/>
+  </React.StrictMode>,
+)
